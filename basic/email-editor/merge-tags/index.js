8a96c788cfd170dfc7935d055aed0be306a37566
Gyula Németh <gyula@chamaileon.io>
2022-05-26 14:25:05 +0200

    merge tags finished

--- basic/email-editor/merge-tags/index.js
+++ basic/email-editor/merge-tags/index.js
@@ -10,96 +10,78 @@
 
 	exampleJsonTextArea.value = JSON.stringify(documentJson)
 
+	const editorInstance = await chamaileonPlugins.createFullscreenPlugin({
+		plugin: 'editor',
+		data: { document: documentJson },
+		settings: {
+			buttons: {
+				textInsert: [
+					{
+						id: 'merge-tags',
+						label: 'Merge tags',
+						icon: 'https://raw.githubusercontent.com/ckeditor/ckeditor4/major/skins/kama/icons/paste.png'
+					}
+				]
+			}
+		},
+		hooks: {
+			onSave: ({ document }) => {
+				exampleJsonTextArea.value = JSON.stringify(document)
+			},
+			onTextInsertPluginButtonClicked: async ({buttonId}) => {
+				if (buttonId !== 'merge-tags') { // this value depends on your configuration. You can configure multiple buttons.
+					return { value: '' }
+				}
+				const mergeTagModal = document.getElementById('merge-tags-modal')
+				mergeTagModal.style.zIndex = 2011
+				mergeTagModal.classList.add('visible')
+
+				function closeMergeTagModal() {
+					mergeTagModal.classList.remove('visible')
+
+					setTimeout(() => {
+						mergeTagModal.style.zIndex = -1
+					}, 500)
+				}
+
+				return new Promise((resolve) => {
+					document.getElementById('merge-tags-modal-close').onclick = () => {
+						resolve({ value: '' }) // don't insert anything
+						closeMergeTagModal()
+					}
+
+					document.getElementById('merge-tags-modal-firstName').onclick = () => {
+						resolve({ value: '%%first_name%%' })
+						closeMergeTagModal()
+					}
+
+					document.getElementById('merge-tags-modal-lastName').onclick = () => {
+						resolve({ value: '%%last_name%%' })
+						closeMergeTagModal()
+					}
+
+					document.getElementById('merge-tags-modal-unsubscribe').onclick = () => {
+						resolve({ value: '%%unsubscribe%%' })
+						closeMergeTagModal()
+					}
+				})
+			},
+			close: () => {
+				editorInstance.hide()
+			}
+		}
+	})
+
 	const showExampleButton = document.getElementById('showExample')
 	showExampleButton.style.display = 'inline-block'
-	showExampleButton.onclick = () => {
+	showExampleButton.onclick = async () => {
 		const documentJson = JSON.parse(exampleJsonTextArea.value)
 
 		documentJson.title = 'demo'
 
-		chamaileonPlugins.editEmail({
-			document: documentJson,
-			settings: {
-				buttons: {
-					textInsert: [
-						{
-							id: 'merge-tags',
-							label: 'Merge tags',
-							icon: 'https://raw.githubusercontent.com/ckeditor/ckeditor4/major/skins/kama/icons/paste.png'
-						}
-					]
-				},
-				elements: {
-					content: {
-						text: true,
-						image: true,
-						button: true,
-						social: true,
-						divider: true,
-						code: true
-					},
-					structure: {
-						box: true,
-						multiColumn: true
-					},
-					advanced: {
-						loop: true,
-						conditional: true,
-						dynamicImage: true
-					}
-				},
-				addons: {
-					blockLock: {
-						enabled: true,
-					},
-					variableSystem: {
-						enabled: true
-					}
-				}
-			},
-			hooks: {
-				onSave: ({ document }) => {
-					exampleJsonTextArea.value = JSON.stringify(document)
-				},
-				onTextInsertPluginButtonClicked: async ({buttonId}) => {
-					if (buttonId !== 'merge-tags') { // this value depends on your configuration. You can configure multiple buttons.
-						return { value: '' }
-					}
-					const mergeTagModal = document.getElementById('merge-tags-modal')
-					mergeTagModal.style.zIndex = 11
-					mergeTagModal.classList.add('visible')
-
-					function closeMergeTagModal() {
-						mergeTagModal.classList.remove('visible')
-
-						setTimeout(() => {
-							mergeTagModal.style.zIndex = -1
-						}, 500)
-					}
-
-					return new Promise((resolve) => {
-						document.getElementById('merge-tags-modal-close').onclick = () => {
-							resolve({ value: '' }) // don't insert anything
-							closeMergeTagModal()
-						}
-
-						document.getElementById('merge-tags-modal-firstName').onclick = () => {
-							resolve({ value: '%%first_name%%' })
-							closeMergeTagModal()
-						}
-
-						document.getElementById('merge-tags-modal-lastName').onclick = () => {
-							resolve({ value: '%%last_name%%' })
-							closeMergeTagModal()
-						}
-
-						document.getElementById('merge-tags-modal-unsubscribe').onclick = () => {
-							resolve({ value: '%%unsubscribe%%' })
-							closeMergeTagModal()
-						}
-					})
-				}
-			}
-		})
+		editorInstance.showSplashScreen()
+		editorInstance.show()
+		await editorInstance.methods.updateData({ document: documentJson })
+		editorInstance.hideSplashScreen()
 	}
 }())
