27d11a26d47f99f3ddb8bcc53e4df12e5842d107
Ladislav Misurak <lacee677@gmail.com>
2023-10-18 10:59:47 +0200

    chore: api version

--- api/generate-email-html/index.js
+++ api/generate-email-html/index.js
@@ -20,7 +20,7 @@
 
 		generatedHtmlTextArea.value = 'loading...'
 
-		const genRequest = await fetch('https://sdk-api.chamaileon.io/api/v1/emails/generate', {
+		const genRequest = await fetch('https://sdk-api.chamaileon.io/api/v2/emails/generate', {
 			method: 'POST',
 			headers: {
 				'Authorization': `Bearer ${apiKey}`,
